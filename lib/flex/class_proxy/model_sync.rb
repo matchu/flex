6d7e3fd6e2845ae978545be67b912323888d1ebb
matchu <matchu1993@gmail.com>
2013-12-09 00:10:18 -0600

    add callbacks option to flex instance proxy's sync
    I want to be able to sync via flex, but I want to delegate it to a background task, so I don't want the callbacks installed. Therefore, take desired callbacks as parameters.

--- lib/flex/class_proxy/model_sync.rb
+++ lib/flex/class_proxy/model_sync.rb
@@ -9,12 +9,14 @@ module Flex
       end
 
       def sync(*synced)
+        options = synced.extract_options!
+        options[:callbacks] ||= [:save, :destroy]
         @synced = synced
         host_class.class_eval do
           raise NotImplementedError, "the class #{self} must implement :after_save and :after_destroy callbacks" \
                 unless respond_to?(:after_save) && respond_to?(:after_destroy)
-          after_save    { flex.sync }
-          after_destroy { flex.sync }
+          after_save    { flex.sync } if options[:callbacks].include? :save
+          after_destroy { flex.sync } if options[:callbacks].include? :destroy
         end
       end
 
